5db3f998aff4bc951f6eabf994de033ff3472566
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-19 12:07:41 -0500

    feat: created new maven project and created main class with a main method and started tp print stuff out but not sure how to use isNumeric yet

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = new String();
+        System.out.println("Enter something:");
+        System.out.println();
+
+    }
+}
